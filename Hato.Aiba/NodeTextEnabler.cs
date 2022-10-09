d728dfb3e1a63d8b38de14d47b9870b73e77783c
Raúl Álvarez <raulwii2@gmail.com>
2022-10-09 12:53:31 +0200

    Basic Setup

---
+++ Hato.Aiba/NodeTextEnabler.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+
+namespace Hato.Aiba
+{
+    public class NodeTextEnabler : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            Melon<NodeTextEnabler>.Logger.Msg("Hello World!");
+        }
+
+        public override void OnUpdate()
+        {
+            
+        }
+    }
+}
