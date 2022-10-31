4e827b6d5a7bae708190d86c9b1a2054bf690c8d
Raúl Álvarez <raulwii2@gmail.com>
2022-10-31 18:51:52 +0100

    Update

--- Hato.Aiba/TextMod.cs
+++ Hato.Aiba/TextMod.cs
@@ -1,11 +1,14 @@
 ﻿using MelonLoader;
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Hato.Aiba
 {
     public class TextMod : MelonMod
     {
+        private string Lastestloaded { get; set; }
+        private bool Isalreadymoded { get; set; }
+
         public override void OnInitializeMelon()
         {
             Melon<TextMod>.Logger.Msg("Aiba cargada!");
@@ -14,20 +17,71 @@ namespace Hato.Aiba
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             base.OnSceneWasLoaded(buildIndex, sceneName);
+            Lastestloaded = sceneName;
+            Isalreadymoded = false;
             Melon<TextMod>.Logger.Msg($"Se ha cargado la scena: {sceneName}");
         }
 
         public override void OnUpdate()
         {
-            var sceneName = SceneManager.GetActiveScene().name;
+            if (!Isalreadymoded)
+                if (Lastestloaded.Equals("flowchart") || Lastestloaded.Equals("file") ||
+                    Lastestloaded.Equals("options"))
+                {
+                    var gameObject = GameObject.Find("UpperGuide/title/Text");
+                    if (gameObject != null) gameObject.SetActive(true);
+                    else Melon<TextMod>.Logger.Msg("No se ha encontrado: UpperGuide/title/Text");
+                    var gameObject2 = GameObject.Find("UpperGuide/title/Text1_root");
+                    if (gameObject2 != null) gameObject2.SetActive(false);
+                    else Melon<TextMod>.Logger.Msg("No se ha encontrado: UpperGuide/title/Text1_root");
+                    TMP_Text text;
+                    if (gameObject != null)
+                        switch (Lastestloaded)
+                        {
+                            case "flowchart":
+                                text = gameObject.GetComponent<TMP_Text>();
+                                if (text != null)
+                                {
+                                    text.SetText("CRONOGRAMA");
+                                    text.color = Color.red;
+                                }
+                                else
+                                {
+                                    Melon<TextMod>.Logger.Msg(
+                                        "No se ha podido encontrar el componente de texto de flowchart");
+                                }
 
-            if (sceneName.Contains("flowchart") || sceneName.Contains("file") || sceneName.Contains("options"))
-            {
-                var gameObject = GameObject.Find("UpperGuide/title/Text");
-                if (gameObject != null) gameObject.SetActive(true);
-                var gameObject2 = GameObject.Find("UpperGuide/title/Text1_root");
-                if (gameObject2 != null) gameObject2.SetActive(false);
-            }
+                                break;
+                            case "file":
+                                text = gameObject.GetComponent<TMP_Text>();
+                                if (text != null)
+                                {
+                                    text.SetText("ARCHIVO");
+                                    text.color = Color.red;
+                                }
+                                else
+                                {
+                                    Melon<TextMod>.Logger.Msg(
+                                        "No se ha podido encontrar el componente de texto de file.");
+                                }
+
+                                break;
+                            case "options":
+                                text = gameObject.GetComponent<TMP_Text>();
+                                if (text != null)
+                                {
+                                    text.SetText("AJUSTES");
+                                    text.color = Color.red;
+                                }
+                                else
+                                {
+                                    Melon<TextMod>.Logger.Msg(
+                                        "No se ha podido encontrar el componente de texto de options.");
+                                }
+
+                                break;
+                        }
+                }
         }
     }
 }
