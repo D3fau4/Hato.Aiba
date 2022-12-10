79c597521c7ab9402e53b76bea0be260a64202a5
Raúl Álvarez <raulwii2@gmail.com>
2022-12-10 11:52:42 +0100

    Update TextMod.cs

--- Hato.Aiba/TextMod.cs
+++ Hato.Aiba/TextMod.cs
@@ -38,124 +38,131 @@ namespace Hato.Aiba
 
         public override void OnUpdate()
         {
-            if (Lastestloaded.Equals("flowchart") || Lastestloaded.Equals("file") ||
-                Lastestloaded.Equals("options"))
+            switch (Lastestloaded)
             {
-                var gameObject = GameObject.Find("UpperGuide/title/Text");
-                if (gameObject != null) gameObject.SetActive(true);
-                else Melon<TextMod>.Logger.Msg("No se ha encontrado: UpperGuide/title/Text");
-                var gameObject2 = GameObject.Find("UpperGuide/title/Text1_root");
-                if (gameObject2 != null) gameObject2.SetActive(false);
-                else Melon<TextMod>.Logger.Msg("No se ha encontrado: UpperGuide/title/Text1_root");
+                case "flowchart":
+                case "file":
+                case "options":
+                {
+                    var gameObject = GameObject.Find("UpperGuide/title/Text");
+                    if (gameObject != null) gameObject.SetActive(true);
+                    else Melon<TextMod>.Logger.Msg("No se ha encontrado: UpperGuide/title/Text");
+                    var gameObject2 = GameObject.Find("UpperGuide/title/Text1_root");
+                    if (gameObject2 != null) gameObject2.SetActive(false);
+                    else Melon<TextMod>.Logger.Msg("No se ha encontrado: UpperGuide/title/Text1_root");
 
-                TMP_Text text;
-                if (gameObject != null)
-                    switch (Lastestloaded)
+                    TMP_Text text;
+                    if (gameObject != null)
+                        switch (Lastestloaded)
+                        {
+                            case "flowchart":
+                                text = gameObject.GetComponent<TMP_Text>();
+                                if (text != null)
+                                {
+                                    text.SetText("CRONOGRAMA");
+                                    text.color = Color.red;
+                                    Isalreadymoded = true;
+                                }
+                                else
+                                {
+                                    Melon<TextMod>.Logger.Msg(
+                                        "No se ha podido encontrar el componente de texto de flowchart");
+                                }
+
+                                break;
+                            case "file":
+                                text = gameObject.GetComponent<TMP_Text>();
+                                if (text != null)
+                                {
+                                    text.SetText("ARCHIVOS");
+                                    text.color = Color.red;
+                                    Isalreadymoded = true;
+                                }
+                                else
+                                {
+                                    Melon<TextMod>.Logger.Msg(
+                                        "No se ha podido encontrar el componente de texto de file.");
+                                }
+
+                                break;
+                            case "options":
+                                text = gameObject.GetComponent<TMP_Text>();
+                                if (text != null)
+                                {
+                                    text.SetText("AJUSTES");
+                                    text.color = Color.red;
+                                    Isalreadymoded = true;
+                                }
+                                else
+                                {
+                                    Melon<TextMod>.Logger.Msg(
+                                        "No se ha podido encontrar el componente de texto de options.");
+                                }
+
+                                break;
+                        }
+
+                    break;
+                }
+                case "save":
+                {
+                    var TagSave = GameObject.Find("TagSave");
+                    if (TagSave != null)
                     {
-                        case "flowchart":
-                            text = gameObject.GetComponent<TMP_Text>();
-                            if (text != null)
-                            {
-                                text.SetText("CRONOGRAMA");
-                                text.color = Color.red;
-                                Isalreadymoded = true;
-                            }
-                            else
-                            {
-                                Melon<TextMod>.Logger.Msg(
-                                    "No se ha podido encontrar el componente de texto de flowchart");
-                            }
-
-                            break;
-                        case "file":
-                            text = gameObject.GetComponent<TMP_Text>();
-                            if (text != null)
-                            {
-                                text.SetText("ARCHIVO");
-                                text.color = Color.red;
-                                Isalreadymoded = true;
-                            }
-                            else
-                            {
-                                Melon<TextMod>.Logger.Msg(
-                                    "No se ha podido encontrar el componente de texto de file.");
-                            }
-
-                            break;
-                        case "options":
-                            text = gameObject.GetComponent<TMP_Text>();
-                            if (text != null)
-                            {
-                                text.SetText("AJUSTES");
-                                text.color = Color.red;
-                                Isalreadymoded = true;
-                            }
-                            else
-                            {
-                                Melon<TextMod>.Logger.Msg(
-                                    "No se ha podido encontrar el componente de texto de options.");
-                            }
-
-                            break;
+                        var textSAVE = TagSave.GetComponent<TMP_Text>();
+                        Melon<TextMod>.Logger.Msg(
+                            $"Tamaño actual de la fuente: {textSAVE.fontSize}");
+                        textSAVE.SetText("GUARDAR");
+                        textSAVE.fontSize = 23f;
+                        TagSave.transform.position = new Vector3(-83.1f,
+                            39.8f, TagSave.transform.position.z);
+                        Melon<TextMod>.Logger.Msg(
+                            $"Tamaño corregido de la fuente: {textSAVE.fontSize}");
+                        Melon<TextMod>.Logger.Msg(
+                            $"Posición del texto: {TagSave.transform.position}");
+                        Isalreadymoded = true;
                     }
-            }
+                    else
+                    {
+                        Melon<TextMod>.Logger.Msg(
+                            "No se ha podido encontrar el componente de texto de save");
+                    }
 
-            if (Lastestloaded.Equals("save"))
-            {
-                var TagSave = GameObject.Find("TagSave");
-                if (TagSave != null)
-                {
-                    var textSAVE = TagSave.GetComponent<TMP_Text>();
-                    Melon<TextMod>.Logger.Msg(
-                        $"Tamaño actual de la fuente: {textSAVE.fontSize}");
-                    textSAVE.SetText("GUARDAR");
-                    textSAVE.fontSize = 23f;
-                    TagSave.transform.position = new Vector3(-83.1f,
-                        39.8f, TagSave.transform.position.z);
-                    Melon<TextMod>.Logger.Msg(
-                        $"Tamaño corregido de la fuente: {textSAVE.fontSize}");
-                    Melon<TextMod>.Logger.Msg(
-                        $"Posición del texto: {TagSave.transform.position}");
-                    Isalreadymoded = true;
-                }
-                else
-                {
-                    Melon<TextMod>.Logger.Msg(
-                        "No se ha podido encontrar el componente de texto de save");
-                }
+                    var TagLoad = GameObject.Find("TagLoad");
+                    if (TagLoad != null)
+                    {
+                        var textLOAD = TagLoad.GetComponent<TMP_Text>();
+                        Melon<TextMod>.Logger.Msg(
+                            $"Tamaño actual de la fuente: {textLOAD.fontSize}");
+                        textLOAD.SetText("CARGAR");
+                        textLOAD.fontSize = 25f;
+                        TagLoad.transform.position = new Vector3(-83.1f,
+                            39.8f, TagLoad.transform.position.z);
+                        Melon<TextMod>.Logger.Msg(
+                            $"Tamaño corregido de la fuente: {textLOAD.fontSize}");
+                        Melon<TextMod>.Logger.Msg(
+                            $"Posición del texto: {textLOAD.transform.position}");
+                        Isalreadymoded = true;
+                    }
+                    else
+                    {
+                        Melon<TextMod>.Logger.Msg(
+                            "No se ha podido encontrar el componente de texto de load");
+                    }
 
-                var TagLoad = GameObject.Find("TagLoad");
-                if (TagLoad != null)
-                {
-                    var textLOAD = TagLoad.GetComponent<TMP_Text>();
-                    Melon<TextMod>.Logger.Msg(
-                        $"Tamaño actual de la fuente: {textLOAD.fontSize}");
-                    textLOAD.SetText("CARGAR");
-                    textLOAD.fontSize = 25f;
-                    TagLoad.transform.position = new Vector3(-83.1f,
-                        39.8f, TagLoad.transform.position.z);
-                    Melon<TextMod>.Logger.Msg(
-                        $"Tamaño corregido de la fuente: {textLOAD.fontSize}");
-                    Melon<TextMod>.Logger.Msg(
-                        $"Posición del texto: {textLOAD.transform.position}");
-                    Isalreadymoded = true;
-                }
-                else
-                {
-                    Melon<TextMod>.Logger.Msg(
-                        "No se ha podido encontrar el componente de texto de load");
-                }
+                    var AutoSave = GameObject.Find("Autosave");
+                    if (AutoSave != null)
+                    {
+                        var autoSave = AutoSave.GetComponent<TMP_Text>();
+                        AutoSave.transform.position = new Vector3(32.6f,
+                            AutoSave.transform.position.y, AutoSave.transform.position.z);
+                        Melon<TextMod>.Logger.Msg(
+                            $"Posición del texto: {AutoSave.transform.position}");
+                        autoSave.SetText("AUTOGUARDADO");
+                        Isalreadymoded = true;
+                    }
 
-                var AutoSave = GameObject.Find("Autosave");
-                if (AutoSave != null)
-                {
-                    var autoSave = AutoSave.GetComponent<TMP_Text>();
-                    AutoSave.transform.position = new Vector3(32.6f,
-                        AutoSave.transform.position.y, AutoSave.transform.position.z);
-                    Melon<TextMod>.Logger.Msg(
-                        $"Posición del texto: {AutoSave.transform.position}");
-                    autoSave.SetText("AUTOGUARDADO");
-                    Isalreadymoded = true;
+                    break;
                 }
             }
         }
